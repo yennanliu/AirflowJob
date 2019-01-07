fd3fe83118a5b25ed0d5a5b75faacfebd4775166
yennanliue <f339339@gmail.com>
2019-01-07 08:52:37 +0000

    add install chromedriver part ref

--- ig/install_instapy.sh
+++ ig/install_instapy.sh
@@ -6,7 +6,12 @@
 #################################################################
 
 
-
+# 1) install dev env 
 yes Y | conda create -n instapy python=3.6
 source activate instapy
+# 2) pull package 
 pip install git+https://github.com/timgrossmann/InstaPy.git
+# 3) install chrome driver 
+# Download chromedriver for your system from here. Extract the .zip file and put it in /assets folder.
+# chromedriver : https://sites.google.com/a/chromium.org/chromedriver/downloads
+# file location :  /Users/yennanliu/anaconda3/lib/python3.6/site-packages/assets
