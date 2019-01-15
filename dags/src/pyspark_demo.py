7d514344a0913fdb5dd28843a587a213fd96a447
yennanliue <f339339@gmail.com>
2019-01-15 08:36:08 +0000

    add src file storage spark scripts

---
+++ dags/src/pyspark_demo.py
@@ -0,0 +1,17 @@
+# spark 
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext, Row
+from operator import add
+# ------------------------------------------------------
+# spark config 
+conf = SparkConf().setAppName("spark dev")
+sc = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
+
+sc
+
+# create RDD
+intRDD = sc.parallelize([6,7,1,2,0])
+intRDD2 = sc.parallelize(["apple", "car", "pan"])
+print (intRDD.collect())
+print (intRDD2.collect())
