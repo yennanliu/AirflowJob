93b8257fb0e52b83e2833a1f1939ad5cdc4f61a1
yennanliue <f339339@gmail.com>
2019-01-15 08:35:44 +0000

    add spark job demo DAG

---
+++ dags/DAG_airflow_spark_job_demo.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
+import os
+
+srcDir = os.getcwd() + '/src/'
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 10, 14, 16, 12),
+    'retries': 5,
+    'retry_delay': timedelta(minutes=1),
+}
+
+with DAG('airflowsparkdemo', default_args=default_args, schedule_interval=timedelta(seconds=45)) as dag:
+
+    start_dag = DummyOperator(task_id='START_dag')
+    spark_job= BashOperator(
+        task_id='spark-job-run',
+        bash_command='python ' + srcDir + 'pyspark_demo.py ',
+        dag=dag)
+    end_dag = DummyOperator(task_id='END_dag')
+
+    start_dag >> spark_job >> end_dag
+
