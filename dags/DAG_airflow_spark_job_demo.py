73812aecf34deec15d2f6629b2528808df49e8e6
yennanliue <f339339@gmail.com>
2019-01-15 09:06:38 +0000

    add airflow-spark-ml job step

--- dags/DAG_airflow_spark_job_demo.py
+++ dags/DAG_airflow_spark_job_demo.py
@@ -19,10 +19,11 @@ srcDir = os.getcwd() + '/src/'
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2016, 10, 14, 16, 12),
+    'start_date': datetime(2019, 01, 01, 16, 12),
     'retries': 5,
     'retry_delay': timedelta(minutes=1),
 }
+
 
 with DAG('airflowsparkdemo', default_args=default_args, schedule_interval=timedelta(seconds=45)) as dag:
 
@@ -31,7 +32,12 @@ with DAG('airflowsparkdemo', default_args=default_args, schedule_interval=timede
         task_id='spark-job-run',
         bash_command='python ' + srcDir + 'pyspark_demo.py ',
         dag=dag)
+    spark_ml_job= BashOperator(
+        task_id='spark-ml_job-run',
+        bash_command='python ' + srcDir + 'Spark_ML_LinearRegression_demo.py ',
+        dag=dag)
     end_dag = DummyOperator(task_id='END_dag')
 
-    start_dag >> spark_job >> end_dag
+    start_dag >> spark_job >> spark_ml_job >> end_dag
+
 
