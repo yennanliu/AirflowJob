c06a2d2aa1863d36850f35a9a9fd077e392aefb1
yennanliue <f339339@gmail.com>
2019-01-22 09:41:03 +0000

    export SPARK_HOME and add print path step

--- dags/DAG_airflow_spark_job_demo.py
+++ dags/DAG_airflow_spark_job_demo.py
@@ -14,7 +14,11 @@ import os
 # https://github.com/danielblazevski/airflow-pyspark-reddit
 
 
-srcDir = os.getcwd() + '/src/'
+os.environ['SPARK_HOME'] = '/spark'
+
+#srcDir = os.getcwd() + '/src/'
+srcDir = '/dags/src/'
+
 
 default_args = {
     'owner': 'airflow',
@@ -28,9 +32,16 @@ default_args = {
 with DAG('airflowsparkdemo', default_args=default_args, schedule_interval=timedelta(seconds=45)) as dag:
 
     start_dag = DummyOperator(task_id='START_dag')
+
+    print_path_env_task = BashOperator(
+        task_id='print_path_env',
+        bash_command='echo $PATH',
+        dag=dag)
+
     spark_job= BashOperator(
         task_id='spark-job-run',
-        bash_command='python ' + srcDir + 'pyspark_demo.py ',
+        #bash_command= 'pwd' + 'ls' + 'ls tmp ' +  'python ' + srcDir + 'pyspark_demo.py ',
+        bash_command= 'pwd && ls && ls /tmp/airflowtmphvku3n3v',
         dag=dag)
     spark_ml_job= BashOperator(
         task_id='spark-ml_job-run',
@@ -38,6 +49,6 @@ with DAG('airflowsparkdemo', default_args=default_args, schedule_interval=timede
         dag=dag)
     end_dag = DummyOperator(task_id='END_dag')
 
-    start_dag >> spark_job >> spark_ml_job >> end_dag
+    start_dag >> print_path_env_task >> spark_job >> spark_ml_job >> end_dag
 
 
