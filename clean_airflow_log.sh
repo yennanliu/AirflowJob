965574233c0ddab46325b22b6af440a944f13371
yennanliue <f339339@gmail.com>
2019-01-26 08:27:36 +0000

    add shell clean up files before airflow restart

---
+++ clean_airflow_log.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#################################################################
+# SHELL CLEAN FILES FROM AIRFLOW BEFORE AIRFLOW REBOOST
+#################################################################
+
+rm airflow.*
+rm -fr logs 
+rm unittests.cfg
