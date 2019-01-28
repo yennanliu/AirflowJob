22955d7e73f1dd7bcaef9b5bdeacf65d4af3b158
yennanliue <f339339@gmail.com>
2019-01-28 08:22:38 +0000

    fix java spark compile and run classpath error and make this DAG run OK

--- dags/DAG_airflow_JAVA_spark_job_demo.py
+++ dags/DAG_airflow_JAVA_spark_job_demo.py
@@ -20,11 +20,11 @@ with DAG('DAG_airflow_JAVA_spark_job_demo', default_args=default_args, schedule_
     start_dag = DummyOperator(task_id='START_dag')
     java_spark_job1_compile= BashOperator(
         task_id='java-ml_job-compile',
-        bash_command='javac -classpath ' + CLASSPATH + ' Spark_java_BasicAvg_demo.java ',
+        bash_command='javac -classpath ' + CLASSPATH +  ' {}Spark_java_BasicAvg_demo.java '.format(srcDir),
         dag=dag)
     java_spark_job1_run= BashOperator(
         task_id='java_ml_job-run',
-        bash_command='java  -classpath ' + CLASSPATH + ' Spark_java_BasicAvg_demo ',
+        bash_command='java  -classpath ' + CLASSPATH +  ' Spark_java_BasicAvg_demo ',
         dag=dag)
     end_dag = DummyOperator(task_id='END_dag')
 
