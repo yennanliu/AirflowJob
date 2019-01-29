201997664b1015f233a0978a59de37243aa80c46
yennanliue <f339339@gmail.com>
2019-01-29 23:03:05 +0000

    deal with case airflow.db file not exists

--- dags/src/get_airflow_job_dag.sh
+++ dags/src/get_airflow_job_dag.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-echo 'GET job list -------------- '
-sqlite3  airflow.db "select * from job;"
-echo 'GET dag list -------------- '
-sqlite3  airflow.db "select * from dag;"
+if [[ -f "airflow.db" ]]; then 
+
+	echo 'airflow.db exists'
+	echo 'GET job list -------------- '
+	sqlite3  airflow.db "select * from job;"
+	echo 'GET dag list -------------- '
+	sqlite3  airflow.db "select * from dag;"
+else 
+	echo 'airflow.db  NOT exists'
+fi 
