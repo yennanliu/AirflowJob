2f995f796944ad728d4870aa1943c8175b903ccb
yennanliue <f339339@gmail.com>
2019-01-29 22:49:00 +0000

    add DAH run shell scripts

---
+++ dags/DAG_airflow_shell_job_demo.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
+import os
+
+srcDir = os.getcwd() + '/dags/src/'
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 10, 14, 16, 12),
+    'retries': 5,
+    'retry_delay': timedelta(minutes=1),
+}
+
+with DAG('DAG_airflow_shell_job_demo', default_args=default_args, schedule_interval=timedelta(seconds=45)) as dag:
+
+    start_dag = DummyOperator(task_id='START_dag')
+    shell_job1= BashOperator(
+        task_id='shell_job1',
+        bash_command= 'bash ' +  srcDir + 'list_all_files.sh',
+        dag=dag)
+    shell_job2= BashOperator(
+        task_id='shell_job2',
+        bash_command='bash '  + srcDir + 'get_airflow_job_dag.sh',
+        dag=dag)
+    end_dag = DummyOperator(task_id='END_dag')
+
+    start_dag >> shell_job1 >> shell_job2 >> end_dag
+
