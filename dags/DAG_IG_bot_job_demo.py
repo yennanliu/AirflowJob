264f4aa6f0adc65ac44c455cd71ccb3543b6ee6c
yennanliue <f339339@gmail.com>
2019-02-06 21:00:16 +0000

    add IG bot cron job demo

---
+++ dags/DAG_IG_bot_job_demo.py
@@ -0,0 +1,61 @@
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator 
+from datetime import datetime, timedelta
+
+
+
+
+# help func 
+def get_country_name(country_name_rates):
+    return list(country_name_rates.keys())[0]
+
+def get_country_rate(country_name_rates): 
+    return float((list(country_name_rates.values()))[0])
+
+
+def IG_demo_job():
+	from instapy import InstaPy
+	from instapy.util import smart_run
+	# login credentials
+	insta_username = insta_username
+	insta_password = insta_password
+	# get an InstaPy session!
+	# set headless_browser=True to run InstaPy in the background
+	session = InstaPy(username=insta_username,
+	                  password=insta_password,
+	                  headless_browser=False)
+	with smart_run(session):
+	    """ Activity flow """
+	    # settings
+	    session.set_relationship_bounds(enabled=True,
+	                                    delimit_by_numbers=True,
+	                                    max_followers=4590,
+	                                    min_followers=45,
+	                                    min_following=77)
+	    session.set_dont_include(["friend1", "friend2", "friend3"])
+	    session.set_dont_like(["pizza", "#store"])
+	    # actions
+	    session.like_by_tags(["natgeo"], amount=10)
+
+
+# airflow DAG 
+args = {
+    'owner': 'yen',
+    'depends_on_past': False,
+    'start_date': datetime(2018, 11, 6),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+    }
+
+
+with DAG(dag_id='DAG_IG_bot_job_demo', default_args=args) as dag:
+	start_dag = DummyOperator(task_id='START_dag')
+	IG_bot_job = PythonOperator(
+	task_id='IG_bot_job',
+	python_callable=IG_demo_job)
+	end_dag = DummyOperator(task_id='END_dag')
+
+	start_dag >> IG_bot_job >> end_dag
