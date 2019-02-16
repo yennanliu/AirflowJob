e573349591c2d488de02eefcb8742224c16ce485
yennanliue <f339339@gmail.com>
2019-02-16 07:25:09 +0000

    roll back run spark job via bash command

--- dags/DAG_astro_airflow_spark_job_demo.py
+++ dags/DAG_astro_airflow_spark_job_demo.py
@@ -6,8 +6,7 @@ from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from datetime import datetime, timedelta
 import os
 import sys 
-from plugins.operators.spark_operator import SPARKOperator
-
+#from src import Spark_ML_RandomForestClassifier_titanic_demo 
 
 #################################################################
 # DAG DEMO RUN SAPRK SCRIPT IN AIRFLOW ETL FRAMEWORK    
@@ -18,8 +17,8 @@ from plugins.operators.spark_operator import SPARKOperator
 # https://github.com/danielblazevski/airflow-pyspark-reddit
 
 
-#os.environ['SPARK_HOME'] = '/usr/bin'
-#sys.path.append(os.path.join(os.environ['SPARK_HOME'], 'bin'))
+os.environ['SPARK_HOME'] = '/usr/local/airflow/'
+sys.path.append(os.environ['SPARK_HOME'])
 
 #srcDir = os.getcwd() + '/src/'
 #srcDir = '/usr/local/airflow/dags/src/'
@@ -33,20 +32,14 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-env = {
-    #'SPARK_HOME': '/spark',
-    'AIRFLOW_HOME' : '/usr/local/airflow'
-    #'JAVA_HOME': '/usr/bin' 
-}
+# env = {
+#     #'SPARK_HOME': '/spark',
+#     #'AIRFLOW_HOME' : '/usr/local/airflow/dags/'
+#     #'JAVA_HOME': '/usr/bin' 
+# }
 
 
-common_spark_args = {
-    'dag_folder': '/dags/src',
-    'env_vars': 'EXECUTION_DATE={{ ds }}'
-}
-
-
-with DAG('DAG_astro_airflow_spark_job_demo', default_args=default_args) as dag:
+with DAG('DAG_astro_airflow_spark_job_demo', default_args=default_args, schedule_interval=timedelta(seconds=45)) as dag:
 
     start_dag = DummyOperator(task_id='START_dag')
 
@@ -65,36 +58,18 @@ with DAG('DAG_astro_airflow_spark_job_demo', default_args=default_args) as dag:
         bash_command='export SPARK_HOME=spark',
         dag=dag)
 
-    # spark_job= BashOperator(
-    #     env=env,
-    #     task_id='spark-job-run',
-    #     #bash_command= 'pwd' + 'ls' + 'ls tmp ' +  'python ' + srcDir + 'pyspark_demo.py ',
-    #     #bash_command= 'pwd && ls && ls /tmp',
-    #     bash_command='export HOME="$(cd ~ && pwd)" && export JAVA_HOME=/usr/bin  && export SPARK_HOME=/spark && echo $HOME $JAVA_HOME $SPARK_HOME && cd  && spark-submit ' + srcDir  + 'pyspark_demo.py ',
-    #     dag=dag)
-    # spark_ml_job= BashOperator(
-    #     task_id='spark-ml_job-run',
-    #     bash_command='python ' + 'Spark_ML_LinearRegression_demo.py ',
-    #     dag=dag)
-
-    spark_job= SPARKOperator(
-        env=env,
+    spark_job= BashOperator(
+        #env=env,
         task_id='spark-job-run',
-        command = 'spark-submit',
-        script_folder= 'pyspark_demo.py',
-        **common_spark_args,
+        #bash_command= 'pwd' + 'ls' + 'ls tmp ' +  'python ' + srcDir + 'pyspark_demo.py ',
+        #bash_command= 'pwd && ls && ls /tmp',
+        bash_command='export HOME="$(cd ~ && pwd)" && export JAVA_HOME=/usr/bin  && export SPARK_HOME=/spark && echo $HOME $JAVA_HOME $SPARK_HOME && cd  && spark-submit ' + srcDir  + 'pyspark_demo.py ',
         dag=dag)
-
-    spark_ml_job= SPARKOperator(
-        env=env,
-        task_id='spark-ml-job-run',
-        command = 'spark-submit',
-        script_folder= 'Spark_ML_LinearRegression_demo.py',
-        **common_spark_args,
+    spark_ml_job= BashOperator(
+        task_id='spark-ml_job-run',
+        bash_command='python ' + srcDir +  'Spark_ML_RandomForestClassifier_titanic_demo.py ',
         dag=dag)
-
     end_dag = DummyOperator(task_id='END_dag')
 
     start_dag >> print_path_env_task >>  print_ls_task >> export_spark_home_task  >> spark_job >> spark_ml_job >> end_dag
-
 
