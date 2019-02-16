24beb488a272d4b2884c7cf1935044b898d3350b
yennanliue <f339339@gmail.com>
2019-02-16 06:32:08 +0000

    wrap all running code with main() func

--- dags/src/Spark_ML_RandomForestClassifier_titanic_demo.py
+++ dags/src/Spark_ML_RandomForestClassifier_titanic_demo.py
@@ -22,14 +22,6 @@ sc = pyspark.SparkContext(conf=conf)
 conf.getAll()
 
 
-# Reading from the hdfs, removing the header
-# read the titanic train, test csv here 
-trainTitanic = sc.textFile( srcDir + "titanic_train.csv")
-# remove the header 
-trainHeader = trainTitanic.first()
-trainTitanic = trainTitanic.filter(lambda line: line != trainHeader).mapPartitions(lambda x: csv.reader(x))
-trainTitanic.first()
- 
 # Data preprocessing
 def sexTransformMapper(elem):
     '''Function which transform "male" into 1 and else things into 0
@@ -41,56 +33,69 @@ def sexTransformMapper(elem):
         return [0]
     else :
         return [1]
- 
-# Data Transformations and filter lines with empty strings
-trainTitanic=trainTitanic.map(lambda line: line[1:3]+sexTransformMapper(line[4])+line[5:11])
-trainTitanic=trainTitanic.filter(lambda line: line[3] != '' ).filter(lambda line: line[4] != '' )
-trainTitanic.take(10)
- 
-# creating "labeled point" rdds specific to MLlib "(label (v1, v2...vp])"
-trainTitanicLP=trainTitanic.map(lambda line: LabeledPoint(line[0],[line[1:5]]))
-trainTitanicLP.first()
- 
-# splitting dataset into train and test set
-# 70% train, 30% test 
-(trainData, testData) = trainTitanicLP.randomSplit([0.7, 0.3])
- 
-# Random forest : same parameters as sklearn (?)
-from pyspark.mllib.tree import RandomForest
- 
-time_start=time.time()
-model_rf = RandomForest.trainClassifier(trainData, numClasses = 2,
-        categoricalFeaturesInfo = {}, numTrees = 100,
-        featureSubsetStrategy='auto', impurity='gini', maxDepth=12,
-        maxBins=32, seed=None)
- 
-  
-model_rf.numTrees()
-model_rf.totalNumNodes()
-time_end=time.time()
-time_rf=(time_end - time_start)
-print("RF takes %d s" %(time_rf))
- 
-# Predictions on test set
-predictions = model_rf.predict(testData.map(lambda x: x.features))
-labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
- 
-# first metrics
-from pyspark.mllib.evaluation import BinaryClassificationMetrics
-metrics = BinaryClassificationMetrics(labelsAndPredictions)
- 
-print ('=====================================================')
-print (' output : ')
+        
+
+def main():
+    # Reading from the hdfs, removing the header
+    # read the titanic train, test csv here 
+    trainTitanic = sc.textFile( srcDir + "titanic_train.csv")
+    # remove the header 
+    trainHeader = trainTitanic.first()
+    trainTitanic = trainTitanic.filter(lambda line: line != trainHeader).mapPartitions(lambda x: csv.reader(x))
+    trainTitanic.first()
+     
+     
+    # Data Transformations and filter lines with empty strings
+    trainTitanic=trainTitanic.map(lambda line: line[1:3]+sexTransformMapper(line[4])+line[5:11])
+    trainTitanic=trainTitanic.filter(lambda line: line[3] != '' ).filter(lambda line: line[4] != '' )
+    trainTitanic.take(10)
+     
+    # creating "labeled point" rdds specific to MLlib "(label (v1, v2...vp])"
+    trainTitanicLP=trainTitanic.map(lambda line: LabeledPoint(line[0],[line[1:5]]))
+    trainTitanicLP.first()
+     
+    # splitting dataset into train and test set
+    # 70% train, 30% test 
+    (trainData, testData) = trainTitanicLP.randomSplit([0.7, 0.3])
+     
+    # Random forest : same parameters as sklearn (?)
+    from pyspark.mllib.tree import RandomForest
+     
+    time_start=time.time()
+    model_rf = RandomForest.trainClassifier(trainData, numClasses = 2,
+            categoricalFeaturesInfo = {}, numTrees = 100,
+            featureSubsetStrategy='auto', impurity='gini', maxDepth=12,
+            maxBins=32, seed=None)
+     
+      
+    model_rf.numTrees()
+    model_rf.totalNumNodes()
+    time_end=time.time()
+    time_rf=(time_end - time_start)
+    print("RF takes %d s" %(time_rf))
+     
+    # Predictions on test set
+    predictions = model_rf.predict(testData.map(lambda x: x.features))
+    labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+     
+    # first metrics
+    from pyspark.mllib.evaluation import BinaryClassificationMetrics
+    metrics = BinaryClassificationMetrics(labelsAndPredictions)
+     
+    print ('=====================================================')
+    print (' output : ')
+
+    # Area under precision-recall curve
+    print("Area under PR = %s" % metrics.areaUnderPR)
+     
+    # Area under ROC curve
+    print("Area under ROC = %s" % metrics.areaUnderROC)
+
+    print ('=====================================================')
 
 
-# Area under precision-recall curve
-print("Area under PR = %s" % metrics.areaUnderPR)
- 
-# Area under ROC curve
-print("Area under ROC = %s" % metrics.areaUnderROC)
 
-print ('=====================================================')
-
-
+if __name__ == '__main__':
+    main()
 
 
--- dags/src/spark_ML_iris_logisticregression.py
+++ dags/src/spark_ML_iris_logisticregression.py
@@ -20,64 +20,68 @@ print ('Python version: {}'.format(sys.version))
 print ('Spark version: {}'.format(spark.version))
 print ('****************')
 
-# load iris.csv into Spark dataframe
-### load iris from sklearn dataset
-from sklearn import datasets
-data =  datasets.load_iris()
-df= pd.DataFrame(data.data)
-df.columns = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width']
-df['class'] = data['target']
-df.to_csv('iris.csv',index=False)
-###
+def main():
+	# load iris.csv into Spark dataframe
+	### load iris from sklearn dataset
+	from sklearn import datasets
+	data =  datasets.load_iris()
+	df= pd.DataFrame(data.data)
+	df.columns = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width']
+	df['class'] = data['target']
+	df.to_csv('iris.csv',index=False)
+	###
 
-data = spark.createDataFrame(pd.read_csv('iris.csv'))
-data = data.withColumn("sepal-length", data["sepal-length"].cast("float"))
-data = data.withColumn("sepal-width", data["sepal-width"].cast("float"))
-data = data.withColumn("petal-length", data["petal-length"].cast("float"))
-data = data.withColumn("petal-width", data["petal-width"].cast("float"))
-data = data.withColumn("class", data["class"].cast("float"))
-print("First 10 rows of Iris dataset:")
-data.show(10)
+	data = spark.createDataFrame(pd.read_csv('iris.csv'))
+	data = data.withColumn("sepal-length", data["sepal-length"].cast("float"))
+	data = data.withColumn("sepal-width", data["sepal-width"].cast("float"))
+	data = data.withColumn("petal-length", data["petal-length"].cast("float"))
+	data = data.withColumn("petal-width", data["petal-width"].cast("float"))
+	data = data.withColumn("class", data["class"].cast("float"))
+	print("First 10 rows of Iris dataset:")
+	data.show(10)
 
-# vectorize all numerical columns into a single feature column
-feature_cols = data.columns[:-1]
-assembler = pyspark.ml.feature.VectorAssembler(inputCols=feature_cols, outputCol='features')
-data = assembler.transform(data)
+	# vectorize all numerical columns into a single feature column
+	feature_cols = data.columns[:-1]
+	assembler = pyspark.ml.feature.VectorAssembler(inputCols=feature_cols, outputCol='features')
+	data = assembler.transform(data)
 
-# convert text labels into indices
-data = data.select(['features', 'class'])
-label_indexer = pyspark.ml.feature.StringIndexer(inputCol='class', outputCol='label').fit(data)
-data = label_indexer.transform(data)
+	# convert text labels into indices
+	data = data.select(['features', 'class'])
+	label_indexer = pyspark.ml.feature.StringIndexer(inputCol='class', outputCol='label').fit(data)
+	data = label_indexer.transform(data)
 
-# only select the features and label column
-data = data.select(['features', 'label'])
-print("Reading for machine learning")
-data.show(10)
+	# only select the features and label column
+	data = data.select(['features', 'label'])
+	print("Reading for machine learning")
+	data.show(10)
 
-# change regularization rate and you will likely get a different accuracy.
-reg = 0.01
-# load regularization rate from argument if present
-if len(sys.argv) > 1:
-    reg = float(sys.argv[1])
+	# change regularization rate and you will likely get a different accuracy.
+	reg = 0.01
+	# load regularization rate from argument if present
+	if len(sys.argv) > 1:
+	    reg = float(sys.argv[1])
 
 
-# use Logistic Regression to train on the training set
-train, test = data.randomSplit([0.70, 0.30])
-lr = pyspark.ml.classification.LogisticRegression(regParam=reg)
-model = lr.fit(train)
+	# use Logistic Regression to train on the training set
+	train, test = data.randomSplit([0.70, 0.30])
+	lr = pyspark.ml.classification.LogisticRegression(regParam=reg)
+	model = lr.fit(train)
 
-# predict on the test set
-prediction = model.transform(test)
-print("Prediction")
-prediction.show(10)
+	# predict on the test set
+	prediction = model.transform(test)
+	print("Prediction")
+	prediction.show(10)
 
-# evaluate the accuracy of the model using the test set
-evaluator = pyspark.ml.evaluation.MulticlassClassificationEvaluator(metricName='accuracy')
-accuracy = evaluator.evaluate(prediction)
+	# evaluate the accuracy of the model using the test set
+	evaluator = pyspark.ml.evaluation.MulticlassClassificationEvaluator(metricName='accuracy')
+	accuracy = evaluator.evaluate(prediction)
 
-print()
-print('#####################################')
-print('Regularization rate is {}'.format(reg))
-print("Accuracy is {}".format(accuracy))
-print('#####################################')
-print()
+	print()
+	print('#####################################')
+	print('Regularization rate is {}'.format(reg))
+	print("Accuracy is {}".format(accuracy))
+	print('#####################################')
+	print()
+
+if __name__ == '__main__':
+	main()
