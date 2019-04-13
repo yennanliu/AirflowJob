3c88a3daac3b3a83d7b4623c58cf0cd27a15907f
yennanliue <f339339@gmail.com>
2019-04-13 08:38:09 +0100

    add operator unittest

---
+++ tests/opertor_unittest.py
@@ -0,0 +1,19 @@
+import unittest
+from datetime import datetime
+from airflow import DAG
+from airflow.models import TaskInstance
+from airflow.operators import MultiplyBy5Operator
+
+
+class TestMultiplyBy5Operator(unittest.TestCase):
+
+    def test_execute(self):
+        dag = DAG(dag_id='anydag', start_date=datetime.now())
+        task = MultiplyBy5Operator(my_operator_param=10, dag=dag, task_id='anytask')
+        ti = TaskInstance(task=task, execution_date=datetime.now())
+        result = task.execute(ti.get_template_context())
+        self.assertEqual(result, 50)
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestMultiplyBy5Operator)
+unittest.TextTestRunner(verbosity=2).run(suite)
