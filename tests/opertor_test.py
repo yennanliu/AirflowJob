023fc94e9ca9ac26f0e28e8ab4a7a1434a9db060
yennanliu <f339339@gmail.com>
2019-06-23 17:16:25 +0800

    update the sample dag for testing

--- tests/opertor_test.py
+++ tests/opertor_test.py
@@ -7,8 +7,8 @@ from airflow.operators import MultiplyBy5Operator
 class TestMultiplyBy5Operator(unittest.TestCase):
 
     def test_execute(self):
-        dag = DAG(dag_id='example_dag', start_date=datetime.now())
-        task = MultiplyBy5Operator(my_operator_param=10, dag=dag, task_id='print_date1')
+        dag = DAG(dag_id='hello_world_dag', start_date=datetime.now())
+        task = MultiplyBy5Operator(my_operator_param=10, dag=dag, task_id='hello_task')
         ti = TaskInstance(task=task, execution_date=datetime.now())
         result = task.execute(ti.get_template_context())
         self.assertEqual(result, 50)
