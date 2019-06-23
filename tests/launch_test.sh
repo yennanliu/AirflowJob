e3cedc1f103eb5be775c3dcd0d1570efef3c636c
yennanliu <f339339@gmail.com>
2019-06-23 16:38:11 +0800

    add shell launch whole airflow tests

---
+++ tests/launch_test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#################################################################
+# SHELL TRIGGER WHOLE AIRFLOW TEST   
+#################################################################
+
+# declare env 
+export AIRFLOW_HOME=/Users/$USER/XJob
+export PYTHONPATH=/Users/$USER/XJob
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# init db (for airflow parameters (saved in DB))
+airflow initdb
+
+#lunch test 
+python -m test 
