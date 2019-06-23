2775c6b6837ce0fbf2b32de55d84a87133f6ff5e
yennanliu <f339339@gmail.com>
2019-06-23 17:15:49 +0800

    fix timedelta import error

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
@@ -6,14 +6,13 @@ from airflow.operators.python_operator import PythonOperator
 def print_hello():
 	return 'Hello Wolrd'
 
- args = {
-		'owner': 'yen',
-		'depends_on_past': False,
-		'start_date': datetime(2018, 12, 30),
-		'retries': 1,
-		'retry_delay': timedelta(minutes=1),
-		'provide_context':True
-		}
+args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 10, 14, 16, 12),
+    'retries': 5,
+    'retry_delay': timedelta(minutes=1),
+}
 
 with DAG(dag_id='hello_world_dag', 
 		 default_args=args) as dag:
