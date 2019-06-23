7fe06dcf3599368e0f16f0b5e4a565cb5d524207
yennanliu <f339339@gmail.com>
2019-06-23 17:40:07 +0800

    print env variables when before launching test

--- tests/launch_test.sh
+++ tests/launch_test.sh
@@ -12,5 +12,8 @@ export LANG=en_US.UTF-8
 # init db (for airflow parameters (saved in DB))
 airflow initdb
 
+# print file structure 
+ls && pwd &&  echo $AIRFLOW_HOME && echo $PYTHONPATH
+
 #lunch test 
 pytest 
