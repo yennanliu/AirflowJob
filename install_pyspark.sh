5ccd4a8ecac34a25aabd8ed61026ccf9fc709ad9
Travis CI <f339339@gmail.com>
2019-07-21 20:21:16 +0800

    fix script code layout

--- install_pyspark.sh
+++ install_pyspark.sh
@@ -23,53 +23,48 @@ $bash /Users/$USER/Xjob/install_pyspark.sh
 COMMENT1
 
 install_spark(){
-
-# exit running the script if any of the following failed (simple command exits with a nonzero exit value) 
-set -e
-# get ops route 
-my_route=$(pwd)
-echo 'my_route :'
-echo $my_route
-echo '>>>>>>>>>>>> STEP 1)  Set up dev env'
-echo 'please change to your dev environment (conda/virtualenv) before running this program, or the spark will be installed in the current environment'
-#yes Y | conda create -n pyspark_dev python=3.5
-#source activate pyspark_dev
-echo '>>>>>>>>>>>> STEP 2)  Install pyspark'
-# download here  : http://spark.apache.org/downloads.html
-cd ~
-wget --quiet http://apache.mirror.anlx.net/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz
-echo 'location :'
-pwd 
-echo 'file :'
-ls 
-tar -xzf $my_route/spark-2.4.3-bin-hadoop2.7.tgz
-cd ~
-cp -R $my_route/spark-2.4.3-bin-hadoop2.7 $my_route/spark
-# install python pyspark library 
-pip install --upgrade pip && pip install pyspark && pip freeze list 
-
-echo '>>>>>>>>>>>> STEP 3)  Declare env parameter'
-# declare env parameter
-export SPARK_HOME=$my_route/spark
-export PATH=$SPARK_HOME/bin:$PATH
+	# exit running the script if any of the following failed (simple command exits with a nonzero exit value) 
+	set -e
+	# get ops route 
+	my_route=$(pwd)
+	echo 'my_route :'
+	echo $my_route
+	echo '>>>>>>>>>>>> STEP 1)  Set up dev env'
+	echo 'please change to your dev environment (conda/virtualenv) before running this program, or the spark will be installed in the current environment'
+	#yes Y | conda create -n pyspark_dev python=3.5
+	#source activate pyspark_dev
+	echo '>>>>>>>>>>>> STEP 2)  Install pyspark'
+	# download here  : http://spark.apache.org/downloads.html
+	cd ~
+	wget --quiet http://apache.mirror.anlx.net/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz
+	echo 'location :'
+	pwd 
+	echo 'file :'
+	ls 
+	tar -xzf $my_route/spark-2.4.3-bin-hadoop2.7.tgz
+	cd ~
+	cp -R $my_route/spark-2.4.3-bin-hadoop2.7 $my_route/spark
+	# install python pyspark library 
+	pip install --upgrade pip && pip install pyspark && pip freeze list 
+	echo '>>>>>>>>>>>> STEP 3)  Declare env parameter'
+	# declare env parameter
+	export SPARK_HOME=$my_route/spark
+	export PATH=$SPARK_HOME/bin:$PATH
 
 }
 
 test_spark_install(){
-
-which pyspark
-which spark-submit 
-
-if  spark-submit dags/src/pyspark_demo.py then 
-
-then 
-	echo '######################## SPARK INSTALL SUCCESS ########################'
-	echo 'PLEASE RUN SPARK VIA BELOW COMMANDS '
-	echo 'export SPARK_HOME=/Users/$USER/spark && export PATH=$SPARK_HOME/bin:$PATH && pyspark'
-	echo '######################## SPARK INSTALL SUCCESS ########################'
-else 
-	echo 'spark NOT install successfully '
-fi 
+	which pyspark
+	which spark-submit 
+	if  spark-submit dags/src/pyspark_demo.py then 
+	then 
+		echo '######################## SPARK INSTALL SUCCESS ########################'
+		echo 'PLEASE RUN SPARK VIA BELOW COMMANDS '
+		echo 'export SPARK_HOME=/Users/$USER/spark && export PATH=$SPARK_HOME/bin:$PATH && pyspark'
+		echo '######################## SPARK INSTALL SUCCESS ########################'
+	else 
+		echo 'spark NOT install successfully '
+	fi 
 }
 
 
